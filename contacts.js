69ee4d4388a4a58dccdcbb480990459e7c61088e
badzlan <badzlandhabith05@gmail.com>
2023-03-20 15:27:13 +0700

    fix any bug

--- contacts.js
+++ contacts.js
@@ -1,21 +1,21 @@
 // Core Module
-const fs = require("fs");
-const readline = require("readline");
-const rl = readline.createInterface({
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
+import { createInterface } from "readline";
+const rl = createInterface({
    input: process.stdin,
    output: process.stdout,
 });
 
 // Create Directory
 const dir = "./data";
-if (!fs.existsSync(dir)) {
-   fs.mkdirSync(dir);
+if (!existsSync(dir)) {
+   mkdirSync(dir);
 }
 
 // Create File JSON
 const data = "./data/contacts.json";
-if (!fs.existsSync(data)) {
-   fs.writeFileSync(data, "[]", "utf-8");
+if (!existsSync(data)) {
+   writeFileSync(data, "[]", "utf-8");
 }
 
 // Question Fuction
@@ -30,15 +30,15 @@ const question = (question) => {
 // Save Contact Function
 const save = (nama, noHP, email) => {
    const contact = { nama: nama, noHP: noHP, email: email };
-   const file = fs.readFileSync("./data/contacts.json", "utf-8");
+   const file = readFileSync("./data/contacts.json", "utf-8");
    const contacts = JSON.parse(file);
 
    contacts.push(contact);
 
-   fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
+   writeFileSync("./data/contacts.json", JSON.stringify(contacts));
 
    console.log("Berhasil menambahkan data!");
    rl.close();
 };
 
-module.exports = { question: question, save: save };
+export {question, save};
---
+++ index.js
@@ -0,0 +1,11 @@
+import { question, save } from "./contacts.js";
+
+const main = async () => {
+   const nama = await question("Masukkan nama anda : ");
+   const noHP = await question("Masukkan no HP anda : ");
+   const email = await question("Masukkan email anda : ");
+
+   save(nama, noHP, email);
+};
+
+main();
