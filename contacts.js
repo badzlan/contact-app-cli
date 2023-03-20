a5f8d18dc3d690296a09f26a9c3522390d3038cd
badzlan <badzlandhabith05@gmail.com>
2023-03-20 15:21:43 +0700

    fix error

--- contacts.js
+++ contacts.js
@@ -1,21 +1,21 @@
 // Core Module
-import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
-import { createInterface } from "readline";
-const rl = createInterface({
+const fs = require("fs");
+const readline = require("readline");
+const rl = readline.createInterface({
    input: process.stdin,
    output: process.stdout,
 });
 
 // Create Directory
 const dir = "./data";
-if (!existsSync(dir)) {
-   mkdirSync(dir);
+if (!fs.existsSync(dir)) {
+   fs.mkdirSync(dir);
 }
 
 // Create File JSON
 const data = "./data/contacts.json";
-if (!existsSync(data)) {
-   writeFileSync(data, "[]", "utf-8");
+if (!fs.existsSync(data)) {
+   fs.writeFileSync(data, "[]", "utf-8");
 }
 
 // Question Fuction
@@ -30,15 +30,15 @@ const question = (question) => {
 // Save Contact Function
 const save = (nama, noHP, email) => {
    const contact = { nama: nama, noHP: noHP, email: email };
-   const file = readFileSync("./data/contacts.json", "utf-8");
+   const file = fs.readFileSync("./data/contacts.json", "utf-8");
    const contacts = JSON.parse(file);
 
    contacts.push(contact);
 
-   writeFileSync("./data/contacts.json", JSON.stringify(contacts));
+   fs.writeFileSync("./data/contacts.json", JSON.stringify(contacts));
 
    console.log("Berhasil menambahkan data!");
    rl.close();
 };
 
-export {question, save};
+module.exports = { question: question, save: save };
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import { question, save } from "./contacts";
+const contacts = require("./contacts");
 
 const main = async () => {
-   const nama = await question("Masukkan nama anda : ");
-   const noHP = await question("Masukkan no HP anda : ");
-   const email = await question("Masukkan email anda : ");
+   const nama = await contacts.question("Masukkan nama anda : ");
+   const noHP = await contacts.question("Masukkan no HP anda : ");
+   const email = await contacts.question("Masukkan email anda : ");
 
-   save(nama, noHP, email);
+   contacts.save(nama, noHP, email);
 };
 
 main();
